2f87ed60e091dccc982b0bfc61de643e57a00660
René Wilhelm <rxw1@pm.me>
2023-08-06 23:29:05 +0200

    CSS

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,38 +1,17 @@
 <script>
   import { onMount } from 'svelte';
   import dayjs from 'dayjs';
-  //let date = 
-  //$: time = (new Date() - date)
-  //let date = new Date();
-  //$: time = new Date()
+  import "./global.css";
 
-
-  let time;
-  let hours = 0;
   let days = 0;
+  let hours = 0;
   let minutes = 0;
   let seconds = 0;
 
-  //let time = new Date() - new Date();
   const date1 = dayjs("2023-08-02T17:20:45");
-  //const date2 = dayjs();
-
-  //$: hours = time.getHours();
-  //$: minutes = time.getMinutes();
-  //$: seconds = time.getSeconds();
-
-
-  //$: hours = date2.diff(date1, 'hours');
-  //$: days = Math.floor(hours / 24);
-  //$: hours2 = hours - (days * 24);
-
-  //console.log('Days: ', days);
-  //console.log('Hours: ', hours);
-
   onMount(() => {
     const interval = setInterval(() => {
       const date2 = dayjs();
-      //time = date2.diff(date1);
       days = date2.diff(date1, 'day')
       hours = date2.subtract(days, 'day').diff(date1, 'hour') 
       minutes = date2.subtract(days, 'day').subtract(hours, 'hour').diff(date1, 'minute')
@@ -45,9 +24,48 @@
   });
 </script>
 
-<span>
-  <div>{days} Tage</div>
-  <div>{hours} Stunden</div>
-  <div>{minutes} Minuten</div>
-  <div>{seconds} Sekunden</div>
-</span>
+<main>
+  <div><span>{days}</span> <span>{days === 1 ? 'Tag' : 'Tage'}</span></div>
+  <div><span>{hours}</span> <span>{hours === 1 ? 'Stunde' : 'Stunden'}</span></div>
+  <div><span>{minutes}</span> <span>{minutes === 1 ? 'Minute' : 'Minuten'}</span></div>
+  <div><span>{seconds}</span> <span>{seconds === 1 ? 'Sekunde' : 'Sekunden'}</span></div>
+</main>
+
+<style>
+  main {
+    width: 800px;
+    max-width: 800px;
+    height: 100vh;
+    display: flex;
+    font-family: 'Lobster', cursive;
+    justify-content: space-around;
+    width: 100%;
+    font-size: 2rem;
+    margin: 5rem 0;
+    color: #ffb;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+  }
+
+  span:first-child {
+    font-size: 5rem;
+    font-weight: bold;
+    animation: fadeIn 1s;
+  }
+
+  @keyframes fadeIn {
+  0% { opacity: 0; }
+  100% { opacity: 1; }
+  }
+
+  span:last-child {
+    font-size: 1.5rem;
+    font-weight: normal;
+    animation: fadeIn 1s;
+  }
+</style>
